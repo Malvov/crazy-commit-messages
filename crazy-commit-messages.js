342dc7f987b29c94080eea09c87bc77679c098d8
malvov <manuel.olivaresbonilla@gmail.com>
2020-02-04 22:12:54 -0600

    retrieving commits from github search api

--- crazy-commit-messages.js
+++ crazy-commit-messages.js
@@ -1,9 +1,20 @@
-const exec = require('child_process').exec
-module.exports = function getCommitMessage() {
+var execSync = require('child_process').execSync
+var getCommitMessage = function getCommitMessage() {
     let keywords = ['shit', 'damn', 'wtf']
     let index = Math.round(Math.random() * 2)
-    let command = ['curl -H "Accept: application/vnd.github.cloak-preview" https://api.github.com/search/commits?q=', keywords[index]].join(' ')
-    return await exec(command, function(error, stderr, stdout) {
-       return stdout
-    })
-}`
+    let command = ['curl -H "Accept: application/vnd.github.cloak-preview" https://api.github.com/search/commits?q=', keywords[index]].join('')
+    let response = execSync(command)
+    let commitObject =  JSON.parse(response)
+    return buildCommitObject(commitObject)
+   
+};
+
+var buildCommitObject = function buildCommitObject(response) {
+    let items = response.items;
+    let index = Math.round(Math.random() * (items.length - 1));
+    let commit = items[index]; 
+    let commitObject = { author: { avatar_url: commit.author.avatar_url, name: commit.commit.author.name }, message: commit.commit.message, date: commit.commit.author.date, url: commit.html_url };
+    return commitObject;
+};
+
+module.exports.getCommitMessage = getCommitMessage;
